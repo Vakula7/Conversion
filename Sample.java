85a14dd846a1274f4b340ff85305792b5573a1c6
Vakula7 <vakulasona96@gmail.com>
2016-08-28 18:21:08 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+public class Sample
+{
+public static void main(String args[])
+{
+int j=10,k;
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+int i=Integer.parseInt(str);
+k=i+j;
+System.out.println(k);
+}
+}
